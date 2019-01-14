0989fd977194036b2951923d13e9e6e381f7fb6a
Contractor [GENESIIS] <contractor@genesiis.com>
2019-01-14 12:37:04 +0530

    master - test commit 1

--- src/test/Employee.java
+++ src/test/Employee.java
@@ -1,10 +1,27 @@
 package test;
 
 public class Employee {
+	String name;
+	int age;
+	
+	public Employee(){}
+	
+	public Employee(String name , int age){
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void printDetails(String name , int age){
+		System.out.println("Employeee Name " + name);
+		System.out.println("Emplyoee Age is :" + age);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Employee name 1");
+		
+		Employee e = new Employee();
+		e.printDetails("Nadee" , 24);
+		
 	}
 
 }
